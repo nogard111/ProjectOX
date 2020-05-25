c2a542cc7846cc634868bc1f1b28734728273f2f
Marcin_Osada <Marcin_Osada@epam.com>
2020-05-25 17:02:33 +0200

    rule book

--- src/main/java/com/github/nogard111/GameEngine.java
+++ src/main/java/com/github/nogard111/GameEngine.java
@@ -1,9 +1,7 @@
 package com.github.nogard111;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
 
 public class GameEngine implements IGameEngine {
     private int GamesToPlay = 3;
@@ -36,13 +34,7 @@ public class GameEngine implements IGameEngine {
         players.put(FieldType.O, new Player(playerOName));
         players.put(FieldType.X, new Player(playerXName));
 
-        fields = new Field[sizeY][sizeX];
-        for (int y = 0; y < fields.length; y++) {
-            var row = fields[y];
-            for (int x = 0; x < row.length; x++) {
-                row[x] = new Field(x, y);
-            }
-        }
+        fields = BoardHelper.GenerateFields(sizeY,sizeX);
 
         currentPlayer = FieldType.O;
     }
@@ -52,7 +44,7 @@ public class GameEngine implements IGameEngine {
         Field selected = getField(x, y);
         if (selected.type == FieldType.NONE) {
             selected.type = currentPlayer;
-            var winners = checkWinners();
+            var winners = GetWinners();
             if (winners != null) {
                 finishStage(winners);
             } else {
@@ -110,50 +102,15 @@ public class GameEngine implements IGameEngine {
     }
 
 
-    private FieldType[] checkWinners() {
-        //Arrays.sort();
-        class WinRule{
-            public int limitRows;
-            public int limitColumn;
-            public int noOfChecks;
-            public Function<Point, Point> getNextPointToCheck;
+    private FieldType[] GetWinners() {
+        WinRule winRules[] = (WinRule[]) BoardHelper.getStandardRules(lenToWin).values().toArray();
 
-
-            public WinRule(int limitRows, int limitColumn,int noOfChecks, Function<Point, Point> getNextPointToCheck) {
-                this.limitRows = limitRows;
-                this.limitColumn = limitColumn;
-                this.noOfChecks = noOfChecks;
-                this.getNextPointToCheck = getNextPointToCheck;
-            }
-        }
-        //public Point NextCheck(Point previous){return }
-        WinRule winRules[] = new WinRule[]{
-                new WinRule(lenToWin,0,lenToWin,(Point k) -> new Point(k.x+1,k.y)),
-                new WinRule(0,lenToWin,lenToWin,(Point k) -> new Point(k.x,k.y+1)),
-                new WinRule(lenToWin,lenToWin,lenToWin,(Point k) -> new Point(k.x+1,k.y+1)),
-                new WinRule(lenToWin,lenToWin,lenToWin,(Point k) -> new Point(k.x+1,k.y))
-        };
-        //todo:hgcg
-           /*
         //check winner
-        for (FieldType playerType:players.keySet()) {
-            boolean win = true;
-            for (int y = 0; y < fields.length; y++) {
-                var row = fields[y];
-                for (int x = 0; x < row.length; x++) {
-
-                    if(row[x].type != playerType ) {
-                        win = false;
-                        break;
-                    }
-                }
-                if(!win)break;
-            }
-            if(win) {
+        for (FieldType playerType : players.keySet()) {
+            if (BoardHelper.IsPlayerAWinner(winRules, playerType, fields)) {
                 return new FieldType[]{playerType};
             }
         }
-*/
 
         //all filled -> tie
         boolean emptyFieldExists = false;
---
+++ src/test/java/com/github/nogard111/RulesTest.java
@@ -0,0 +1,45 @@
+package com.github.nogard111;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RulesTest {
+
+    /**
+     * Vertical Test
+     */
+    @Test
+    public void verticalRule()
+    {
+        var rule = BoardHelper.getStandardRules(3).get(BoardHelper.WinRuleType.VERTICAL);
+
+        Field[][] fields = BoardHelper.GenerateFields(3,3);
+
+        fields[0][1].type = FieldType.O;
+        fields[0][1].type = FieldType.O;
+        fields[0][1].type = FieldType.O;
+
+
+        assertTrue(BoardHelper.IsPlayerAWinner(new WinRule[]{rule}, FieldType.O, fields));
+    }
+
+    /**
+     * Horizontal Test
+     */
+    @Test
+    public void horizontalRule()
+    {
+        var rule = BoardHelper.getStandardRules(3).get(BoardHelper.WinRuleType.HORIZONTAL);
+
+        Field[][] fields = BoardHelper.GenerateFields(3,3);
+
+        fields[0][0].type = FieldType.O;
+        fields[1][0].type = FieldType.O;
+        fields[2][0].type = FieldType.O;
+
+
+        assertTrue(BoardHelper.IsPlayerAWinner(new WinRule[]{rule}, FieldType.O, fields));
+    }
+}
